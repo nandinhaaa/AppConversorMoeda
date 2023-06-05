4fa9784de457566ad7ab12dafe8d6560c570bfec
Nanda <mariafernandaleee@gmail.com>
2023-06-05 16:45:35 -0300

    main

--- app/src/main/java/com/example/conversorapp/InfoActivity.java
+++ app/src/main/java/com/example/conversorapp/InfoActivity.java
@@ -1,6 +1,10 @@
 package com.example.conversorapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,11 +23,27 @@ public class InfoActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TextView linkTextView = findViewById(R.id.linkTextView);
+        linkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir o link desejado
+                String url = "https://www.dolarhoje.net.br/dolar-comercial/#:~:text=Aqui%20no%20site%20DolarHoje.Net.Br%20voc%C3%AA%20encontrar%20informa%C3%A7%C3%B5es%20atualizadas,comercial%2Fturismo%20e%20outras%20moedas%20como%20Euro%20e%20Pesos";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle("Info");
+            actionBar.setTitle("Informações");
         }
+
+
     }
+
 }
 
